f55952e63904949cc2790def75ee7448e71c9e64
jrrodrigues <rodrigues.jro@gmail.com>
2022-12-10 23:06:19 -0300

    first commit

---
+++ src/main/java/br/leg/camara/ditec/copad/App.java
@@ -0,0 +1,13 @@
+package br.leg.camara.ditec.copad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
